b9cac72b56e029771208d7805e3012a9af44ae1f
C <45455218+crypblizz8@users.noreply.github.com>
2022-04-21 16:23:07 +1000

    fix header
    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,39 +6,43 @@ import Wallet from "./wallet";
 
 export default function Header() {
   return (
-    <Head>
-      <title>lil pragma</title>
-      <meta
-        name="description"
-        content="smoll web3 projects with a lil dev score"
-      />
-      <link rel="icon" href="/favicon.ico" />
+    <>
+      <Head>
+        <title>lil pragma</title>
+        <meta
+          name="description"
+          content="smoll web3 projects with a lil dev score"
+        />
+        <link rel="icon" href="/favicon.ico" />
 
-      <meta property="og:url" content="https://www.lilpragma.xyz/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="lil pragma" />
-      <meta
-        property="og:description"
-        content="smoll web3 projects with a lil dev score"
-      />
-      <meta
-        property="og:image"
-        content="https://www.lilpragma.xyz/ogImage.png"
-      />
+        <meta property="og:url" content="https://www.lilpragma.xyz/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="lil pragma" />
+        <meta
+          property="og:description"
+          content="smoll web3 projects with a lil dev score"
+        />
+        <meta
+          property="og:image"
+          content="https://www.lilpragma.xyz/ogImage.png"
+        />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="lilpragma.xyz" />
-      <meta property="twitter:url" content="https://www.lilpragma.xyz/" />
-      <meta name="twitter:title" content="lil pragma" />
-      <meta
-        name="twitter:description"
-        content="smoll web3 projects with a lil dev score"
-      />
-      <meta
-        name="twitter:image"
-        content="https://www.lilpragma.xyz/ogImage.png"
-      />
-      <Wallet />
-    </Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="lilpragma.xyz" />
+        <meta property="twitter:url" content="https://www.lilpragma.xyz/" />
+        <meta name="twitter:title" content="lil pragma" />
+        <meta
+          name="twitter:description"
+          content="smoll web3 projects with a lil dev score"
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.lilpragma.xyz/ogImage.png"
+        />
+      </Head>
+      <div>
+        <Wallet />
+      </div>
+    </>
   );
 }
